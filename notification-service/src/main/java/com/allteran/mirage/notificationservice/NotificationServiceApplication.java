ff44980968137aa1dd4d21f0ca23d05aafb75055
Vitalii Prozapas <vitalii.prozapas@gmail.com>
2022-04-20 16:55:56 +0300

    Enable Notification Service with RabbitMQ. Implemented consumer&producer of messages

---
+++ notification-service/src/main/java/com/allteran/mirage/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,22 @@
+package com.allteran.mirage.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@Bean
+	public Consumer<String> notificationEventSupplier() {
+		return message -> {
+			System.out.println("NOTIFICATION SERVICE RECEIVED message: " + message);
+		};
+	}
+}
--- workshop-service/src/main/java/com/allteran/mirage/workshopservice/controller/OrderController.java
+++ workshop-service/src/main/java/com/allteran/mirage/workshopservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.allteran.mirage.workshopservice.client.RootClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,14 @@ import java.util.function.Supplier;
 public class OrderController {
     private final RootClient rootClient;
     private final Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+    private final StreamBridge streamBridge;
+    private static final String ORDER_BINDING_NAME = "notificationEventSupplier-out-0";
 
     @Autowired
-    public OrderController(RootClient rootClient, Resilience4JCircuitBreakerFactory circuitBreakerFactory) {
+    public OrderController(RootClient rootClient, Resilience4JCircuitBreakerFactory circuitBreakerFactory, StreamBridge streamBridge) {
         this.rootClient = rootClient;
         this.circuitBreakerFactory = circuitBreakerFactory;
+        this.streamBridge = streamBridge;
     }
 
     @GetMapping("test/{id}")
@@ -34,6 +38,7 @@ public class OrderController {
         Supplier<String> resultStringSupplier = rootClient::getTestData;
 
         String resultString = circuitBreaker.run(resultStringSupplier, this::handleErrorCase);
+        streamBridge.send(ORDER_BINDING_NAME, "testIdNoti");
         return resultString;
     }
 
